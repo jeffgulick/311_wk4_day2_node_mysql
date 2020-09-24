6fdd0320e2c2dc41274b83a2896a44d5ce6b06ec
JEFFGULICK <jeffgulick@gmail.com>
2020-09-24 00:11:42 -0500

    fixed formatting on getAllUsers

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,9 @@ const { handleSQLError } = require('../sql/error')
 const getAllUsers = (req, res) => {
   // SELECT ALL USERS
   pool.query(
-    "SELECT * FROM users JOIN usersContact ON users.id = usersContact.user_id JOIN usersAddress ON users.id = usersAddress.user_id", 
+    `SELECT * FROM users 
+    JOIN usersContact ON users.id = usersContact.user_id 
+    JOIN usersAddress ON users.id = usersAddress.user_id`, 
     
     (err, rows) => {
     if (err) return handleSQLError(res, err)
